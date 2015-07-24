782cd9014818953fb66868f7badd4e8a3671801b
Stacey Vetzal <stacey@vetzal.com>
2015-07-23 23:25:48 -0400

    adding tests to stage3

---
+++ Stage3/specs/group_list_controller_spec.js
@@ -0,0 +1,26 @@
+﻿/// <reference path="../Scripts/jasmine.js" />
+/// <reference path="../Scripts/angular.js" />
+/// <reference path="../Scripts/angular-route.js" />
+/// <reference path="../Scripts/angular-mocks.js" />
+
+/// <reference path="../App/main.js" />
+
+describe("main", function () {
+    
+    beforeEach(module("main"));
+
+    describe("GroupListController", function () {
+
+        var controller;
+
+        beforeEach(inject(function ($controller) {
+            controller = $controller('GroupListController');
+        }));
+
+        it("should have a list of group phone numbers", function () {
+            //expect(controller.groups.length).toBeGreaterThan(3);
+        });
+
+    });
+
+});
---
+++ Stage3/specs/phone_formatter_filter_spec.js
@@ -0,0 +1,27 @@
+﻿/// <reference path="../Scripts/jasmine.js" />
+/// <reference path="../Scripts/angular.js" />
+/// <reference path="../Scripts/angular-route.js" />
+/// <reference path="../Scripts/angular-mocks.js" />
+
+/// <reference path="../App/main.js" />
+
+describe("main", function () {
+    
+    beforeEach(module("main"));
+
+    describe("phoneFormatter", function () {
+
+        var filter;
+
+        beforeEach(inject(function ($filter) {
+            filter = $filter('phoneFormatter');
+        }));
+
+        it("should present a properly formatted phone number", function () {
+            var formattedPhone = filter('+19055551212', 'phoneFormatter');
+            expect(formattedPhone).toEqual("(905) 555-1212");
+        });
+
+    });
+
+});
